13abba3feddb060feb2408e50797e02fd9fbd353
Miro Palmu <email@miropalmu.cc>
2025-08-31 17:04:09 +0300

    tonttu: enhance adjust layer

--- keyboards/splitkb/halcyon/elora/keymaps/tonttu_hlc/keymap.c
+++ keyboards/splitkb/halcyon/elora/keymaps/tonttu_hlc/keymap.c
@@ -21,12 +21,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: physical QWERTY layout
  */
+
     [_QWERTY] = LAYOUT_elora_hlc(
-     KC_TAB  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_GRV ,
+     ADJUST  , KC_1 ,  KC_2   ,  KC_3  ,   KC_4 ,   KC_5 ,                                        KC_6 ,  KC_7 ,  KC_8 ,   KC_9 ,  KC_0 , KC_GRV ,
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      KC_ESC  , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SLSH, KC_ENT ,
      KC_LCTL , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,QWERTY_EXT, KC_NO   , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_MINS, KC_EQL ,
-                                ADJUST , KC_LGUI, KC_LALT, KC_SPC ,KC_LSFT   , KC_RSFT , KC_SPC ,KC_SCLN, KC_QUOT,KC_BSLS,
+                                 KC_NO , KC_LGUI, KC_LALT, KC_SPC ,KC_LSFT   , KC_RSFT , KC_SPC ,KC_SCLN, KC_QUOT,KC_BSLS,
      KC_MUTE, KC_NO,  KC_NO, KC_NO, KC_NO,                                                                KC_MUTE, KC_NO, KC_NO, KC_NO, KC_NO
     ),
 /*
@@ -35,13 +36,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |  F12   |  F1  |  F2  |  F3  |  F4  |  F5  |                              |  F6  |  F7  |  F8  |  F9  | F10  |  F11   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |                              | PgUp | Home |   ↑  | End  | VolUp| Delete |
+ * |  PB1   | PB2  | PB3  | PB4  | PB5  | PB6  |                              | PgUp | Home |   ↑  | End  | VolUp| Delete |
  * |--------+------+------+------+------+------|             MW = Mouse Wheel |------+------+------+------+------+--------|
- * |        |  GUI |  Alt | Ctrl | Shift|      |             MS = MouSe       | PgDn |  ←   |   ↓  |   →  | VolDn| Insert |
+ * |  PB7   | PB8  | PB9  | PB10 | PB11 | PB12 |             MS = MouSe       | PgDn |  ←   |   ↓  |   →  | VolDn| Insert |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |ScLck |  |      | MWUP | Pause|M Prev|M Play|M Next|VolMut| PrtSc  |
+ * |  PB13  | PB14 | PB15 | PB16 | PB17 | PB18 | PB19 |      |  |      | MWUP | Pause|M Prev|M Play|M Next|VolMut| PrtSc  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  | MS1  | MWDO | MS2  |      |      |
+ *                        | PB20 | PB21 | PB22 | PB23 | PB24 |  | MS1  | MWDO | MS2  |      |      |
  *                        `----------------------------------'  `----------------------------------'
  * ,-----------------------------------.                                              ,-----------------------------------.
  * |      |      |       |      |      |                                              |      |      |       |      |      |
@@ -49,10 +50,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
     [_QWERTY_EXT] = LAYOUT_elora_hlc(
       KC_F12 ,  KC_F1 , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                                     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11,
-      _______, _______, _______, _______, _______, _______,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
-      _______, KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
-      _______, _______, _______, _______, _______, _______, _______, KC_SCRL, _______, MS_WHLU,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
-                                 _______, _______, _______, _______, _______, MS_BTN1, MS_WHLD, MS_BTN2, _______, _______,
+      PB_1   , PB_2   , PB_3   , PB_4   , PB_5   , PB_6   ,                                     KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_VOLU, KC_DEL,
+      PB_7   , PB_8   , PB_9   , PB_10  , PB_11  , PB_12  ,                                     KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_VOLD, KC_INS,
+      PB_13  , PB_14  , PB_15  , PB_16  , PB_17  , PB_18  , PB_19  , _______, _______, MS_WHLU,KC_PAUSE, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_PSCR,
+                                 PB_20  , PB_21  , PB_22  , PB_23  , PB_24  , MS_BTN1, MS_WHLD, MS_BTN2, _______, _______,
      _______, _______,  _______, _______, _______,                                                       _______, _______, _______, _______, _______
     ),
 
@@ -60,27 +61,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Adjust Layer: Default layer settings, RGB
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |      |      |      |      |      |                              |      |      |      |      |      |        |
+ * |        |      |      |      |      |      |                              | MAC0 | MAC1 | MAC2 |      |      |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |      | MAC0 |      | MAC = Mouse ACceleration     |      |      |      |      |      |        |
+ * |        |      |      |      |      |      | MAC = Mouse ACceleration     |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |        |      |      |      | MAC1 |      |                              | TOG  | SAI  | HUI  | VAI  | MOD  |        |
+ * |        |      |      |      |      |      |                              | TOG  | SAI  | HUI  | VAI  | MOD  |        |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |        |      |      |      | MAC2 |      |      |      |  |      |      |      | SAD  | HUD  | VAD  | RMOD |        |
+ * |        |      |      |      |      |      |      |      |  |      |      |      | SAD  | HUD  | VAD  | RMOD |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |      |      |      |      |      |  |      |      |      |      |      |
- *                        |      |      |      |      |      |  |      |      |      |      |      |
+ *                        |boot- | boot |      |      |      |  |      |      |      |      |      |
+ *                        |loader|      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  * ,-----------------------------------.                                              ,-----------------------------------.
  * |      |      |       |      |      |                                              |      |      |       |      |      |
  * `-----------------------------------'                                              `-----------------------------------'
  */
     [_ADJUST] = LAYOUT_elora_hlc(
+      _______, _______, _______, _______, _______, _______,                                    MS_ACL0, MS_ACL1, MS_ACL2, _______, _______, _______,
       _______, _______, _______, _______, _______, _______,                                    _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, MS_ACL0, _______,                                    _______, _______, _______, _______, _______, _______,
-      _______, _______, _______, _______, MS_ACL1, _______,                                    RM_TOGG, RM_SATU, RM_HUEU, RM_VALU, RM_NEXT, _______,
-      _______, _______, _______, _______, MS_ACL2, _______,_______, _______, _______, _______, _______, RM_SATD, RM_HUED, RM_VALD, RM_PREV, _______,
-                                 _______, _______, _______,_______, _______, _______, _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______,                                    RM_TOGG, RM_SATU, RM_HUEU, RM_VALU, RM_NEXT, _______,
+      _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______, RM_SATD, RM_HUED, RM_VALD, RM_PREV, _______,
+                                 QK_BOOT,  QK_RBT, _______,_______, _______, _______, _______, _______, _______, _______,
      _______, _______,  _______, _______, _______,                                                      _______, _______, _______, _______, _______
     ),
 
